dccb36e5d119ed157aabd2f464d673fd434981aa
tsotne1 <tsotne.mazmishvili.1@btu.edu.ge>
2020-02-26 02:16:41 +0400

    ajax request javascript response and foodmeals controller

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,7 +5,7 @@ class Stock < ApplicationRecord
   validates :name, :ticker, presence: true
 
   def self.new_lookup(ticker_symbol)
-    client = IEX::Api::Client.new(publishable_token: 'Tpk_9827031e33ef408aa38db47a3aa396cd',
+    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
                                   endpoint: 'https://sandbox.iexapis.com/v1')
      begin
        new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
